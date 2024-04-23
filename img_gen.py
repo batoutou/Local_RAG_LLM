e2e5691332cfe0dbd24d7a768f78cf639335c971
Baptiste <baptistee@kickmaker.net>
2024-04-23 09:51:07 +0200

    adding stable diffusion for image generation

--- img_gen.py
+++ img_gen.py
@@ -1,19 +1,25 @@
 import torch
-from stable-diffusion import StableDiffusion
+from diffusers import DiffusionPipeline, DDIMScheduler
+from huggingface_hub import hf_hub_download
 
 # Define the text prompt
 prompt = "A beautiful sunset on a beach"
 
 # Initialize the Stable Diffusion model
-model = StableDiffusion()
+base_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+repo_name = "ByteDance/Hyper-SD"
+# Take 2-steps lora as an example
+ckpt_name = "Hyper-SDXL-2steps-lora.safetensors"
+# Load model.
+pipe = DiffusionPipeline.from_pretrained(base_model_id, torch_dtype=torch.float16, variant="fp16").to("cuda")
+pipe.load_lora_weights(hf_hub_download(repo_name, ckpt_name))
+pipe.fuse_lora()
 
-# Set the hyperparameters for the diffusion process
-num_steps = 1000
-step_size = 0.1
-temperature = 0.5
+# Ensure ddim scheduler timestep spacing set as trailing !!!
+pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
 
 # Generate the image
-image = model.generate(prompt, num_steps, step_size, temperature)
+image=pipe(prompt=prompt, num_inference_steps=2, guidance_scale=0).images[0]
 
 # Display the generated image
 import matplotlib.pyplot as plt
